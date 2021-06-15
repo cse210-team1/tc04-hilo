5adc2bbe1cfd867005d470eacf08cd71966a1b77
David Bloomfield <david.s.bloomfield@gmail.com>
2021-06-15 16:47:46 -0600

    added methods to director

---
+++ hilo/game/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+
+""" Contains modules for game play"""
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,12 +1,34 @@
-class director:
+from game.dealer import dealer
+
+class Director:
     def __init__(self):
-        pass
+        
+        self.dealer = dealer()
         
     def game_run(self):
-        pass
+        while can_play():
+            
+            get_inputs()
+            do_outputs()
+            if input("Play again? [y/n]") == "n":
+                break
+            
 
     def get_inputs(self):
-        pass
+        """ Gets a cards from the dealer, prints the card, and asks the user for a guess"""
+        
+        self.card = self.dealer.draw_card()
+        print(f"The card is: {self.card}")
+        self.guess = input("Higher or Lower? [h/l] ")
 
-    def get_outputs(self):
-        pass
+    def do_outputs(self):
+        """ Gets the socre from the dealer and prints it"""
+        self.score = self.dealer.calculate_points()
+        print(f"Your Score is: {score}")
+            
+    def can_play():
+        if self.score <= 0:
+            return False
+        else:
+            return True
+        
