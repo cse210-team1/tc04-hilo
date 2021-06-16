c471c8c23a5d0e652d1f12e7bf0605cb48421384
Teetothejay13 <teetothejay13@gmail.com>
2021-06-15 20:32:41 -0600

    update to dealer.draw_card()

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,10 +1,14 @@
+import random
+
 class Dealer:
+    # Define the points and initialize the card
     def __init__(self):
         self.points = 300
-        self.card = 0
-
+        self.current_card = 0
+    # Generate a random card to draw and return
     def draw_card(self):
-        pass
-
-    def calculate_points(self):
+        self.card = random.randint(1, 13)
+        return self.current_card
+    # Take the user guess and calculate their points after gaining/losing
+    def calculate_points(self, guess):
         pass
