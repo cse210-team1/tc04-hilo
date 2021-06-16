ee86684b54760354b737d87c07f74174ea5f2390
Teetothejay13 <teetothejay13@gmail.com>
2021-06-15 20:48:31 -0600

    adding previous_card to dealer

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -5,9 +5,11 @@ class Dealer:
     def __init__(self):
         self.points = 300
         self.current_card = 0
+        self.previous_card = 0
     # Generate a random card to draw and return
     def draw_card(self):
-        self.card = random.randint(1, 13)
+        self.previous_card = self.current_card
+        self.current_card = random.randint(1, 13)
         return self.current_card
     # Take the user guess and calculate their points after gaining/losing
     def calculate_points(self, guess):
