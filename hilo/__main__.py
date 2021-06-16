f9b966d1f31132de313cbb71c2005c5483b79651
David Bloomfield <david.s.bloomfield@gmail.com>
2021-06-15 21:51:07 -0600

    made game play match example

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -1,1 +1,8 @@
+from game.director import Director
 
+def main():
+	director = Director()
+	director.game_run()
+	
+if __name__ == "__main__":
+	main()
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,30 +1,31 @@
-from game.dealer import dealer
+from game.dealer import Dealer
 
 
 class Director:
     def __init__(self):
-        self.dealer = dealer()
+        self.dealer = Dealer()
         self.guess = ""
         self.score = 300
 
     def game_run(self):
         while self.can_play():
-
             self.get_inputs()
             self.do_outputs()
-            if input("Play again? [y/n]") == "n":
+            if input("Play again? [y/n] ") != "y":
                 break
-
+            print()
     def get_inputs(self):
         """ Gets a cards from the dealer, prints the card, and asks the user for a guess"""
 
-        self.card = self.dealer.draw_card()
-        print(f"The card is: {self.card}")
+        card = self.dealer.draw_card()
+        print(f"The card is: {card}")
         self.guess = input("Higher or Lower? [h/l] ")
+        next_card = self.dealer.draw_card()
+        print(f"The next card is: {next_card}")
 
     def do_outputs(self):
         """ Gets the socre from the dealer and prints it"""
-        self.score = self.dealer.calculate_points(self.guess)
+        self.score += self.dealer.calculate_points(self.guess)
         # If their score is 0 or below, end the game
         print(f"Your Score is: {self.score}")
 
